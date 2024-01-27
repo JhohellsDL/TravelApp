f5baf5a20094ebc04add278ea272772cfa127bba
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2024-01-26 20:13:58 -0500

    commit test

--- app/src/main/java/com/example/travelapp/MainActivity2.kt
+++ app/src/main/java/com/example/travelapp/MainActivity2.kt
@@ -2,10 +2,25 @@ package com.example.travelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import com.example.travelapp.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    private var isVisible = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageVisibility.setOnClickListener {
+            val currentInputType = binding.editTextPassword.inputType
+            binding.editTextPassword.inputType = if (currentInputType == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            } else {
+                InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            }
+            binding.editTextPassword.setSelection(binding.editTextPassword.text.length)
+        }
     }
 }
