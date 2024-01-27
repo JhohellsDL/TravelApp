d1b58b90d86205d4236ae978fe310a299d359529
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2024-01-26 21:26:33 -0500

    animation add

--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.travelapp
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.travelapp.databinding.ActivityMainBinding
+
+
+private const val ANIMATION_DURATION = 1000L
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,9 +23,59 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        moveTwoText(binding.textText1, binding.textText2)
+        binding.cardButton.fadeInAndMoveUp()
+
         binding.buttonNext.setOnClickListener {
             startActivity(Intent(this@MainActivity, MainActivity2::class.java))
         }
 
+    }
+
+    private fun moveTwoText(textView1: TextView, textView2: TextView) {
+        textView1.alpha = 0f
+        textView2.alpha = 0f
+
+        val fadeInAnimator1 = ObjectAnimator.ofFloat(textView1, "alpha", 0f, 1f)
+        fadeInAnimator1.duration = ANIMATION_DURATION
+        val fadeInAnimator2 = ObjectAnimator.ofFloat(textView2, "alpha", 0f, 1f)
+        fadeInAnimator2.duration = ANIMATION_DURATION
+
+        val animator1 = ObjectAnimator.ofFloat(textView1, "translationY", 0f, 10f)
+        animator1.duration = ANIMATION_DURATION
+        val animator2 = ObjectAnimator.ofFloat(textView2, "translationY", 0f, 10f)
+        animator2.duration = ANIMATION_DURATION
+
+        animator1.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                animator2.start()
+                fadeInAnimator2.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+
+        fadeInAnimator1.start()
+        animator1.start()
+    }
+
+    private fun CardView.fadeInAndMoveUp() {
+        val fadeInAnimator = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f)
+        fadeInAnimator.duration = ANIMATION_DURATION
+
+        val translateAnimator = ObjectAnimator.ofFloat(this, "translationY", 0f, -20f)
+        fadeInAnimator.duration = ANIMATION_DURATION
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(fadeInAnimator, translateAnimator)
+        animatorSet.start()
     }
 }
--- app/src/main/java/com/example/travelapp/MainActivity2.kt
+++ app/src/main/java/com/example/travelapp/MainActivity2.kt
@@ -1,17 +1,25 @@
 package com.example.travelapp
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.travelapp.databinding.ActivityMain2Binding
 
+
+private const val ANIMATION_DURATION = 1000L
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
-    private var isVisible = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
         binding.imageVisibility.setOnClickListener {
             val currentInputType = binding.editTextPassword.inputType
@@ -22,5 +30,141 @@ class MainActivity2 : AppCompatActivity() {
             }
             binding.editTextPassword.setSelection(binding.editTextPassword.text.length)
         }
+        binding.cardButtonCreate.setOnClickListener {
+            binding.textTitle.fadeInAndMoveUpAndChanged()
+            moveTwoText(
+                binding.cardEmail,
+                binding.cardPassword,
+                binding.cardButtonLogin
+            )
+
+        }
     }
+    private fun TextView.fadeInAndMoveUpAndChanged() {
+        alpha = 0f
+        translationY = 0f
+
+        val fadeInAnimator = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f)
+        fadeInAnimator.duration = ANIMATION_DURATION
+
+        val moveUpAnimator = ObjectAnimator.ofFloat(this, "translationX", -30f, 10f)
+        moveUpAnimator.duration = ANIMATION_DURATION
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(fadeInAnimator, moveUpAnimator)
+        animatorSet.start()
+    }
+
+    private fun moveTwoText(cardView1: CardView, cardView2: CardView, cardView3: CardView) {
+        cardView1.alpha = 0f
+        cardView2.alpha = 0f
+        cardView3.alpha = 0f
+
+        binding.cardForgot.alpha = 0f
+        binding.cardCreate.alpha = 0f
+
+        val fadeInAnimator1 = ObjectAnimator.ofFloat(cardView1, "alpha", 0f, 1f)
+        fadeInAnimator1.duration = ANIMATION_DURATION
+        val fadeInAnimator2 = ObjectAnimator.ofFloat(cardView2, "alpha", 0f, 1f)
+        fadeInAnimator2.duration = ANIMATION_DURATION
+        val fadeInAnimator3 = ObjectAnimator.ofFloat(cardView3, "alpha", 0f, 1f)
+        fadeInAnimator2.duration = ANIMATION_DURATION
+
+        val animator1 = ObjectAnimator.ofFloat(cardView1, "translationX", -40f, 10f)
+        animator1.duration = ANIMATION_DURATION
+        val animator2 = ObjectAnimator.ofFloat(cardView2, "translationX", -40f, 10f)
+        animator2.duration = ANIMATION_DURATION
+        val animator3 = ObjectAnimator.ofFloat(cardView3, "translationX", -40f, 10f)
+        animator2.duration = ANIMATION_DURATION
+
+        animator1.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                animator2.start()
+                fadeInAnimator2.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+        animator2.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                animator3.start()
+                fadeInAnimator3.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+        animator3.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                moveTwoCard(
+                    binding.cardForgot,
+                    binding.cardCreate
+                )
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+
+        fadeInAnimator1.start()
+        animator1.start()
+    }
+
+    private fun moveTwoCard(cardView1: CardView, cardView2: CardView) {
+        cardView1.alpha = 0f
+        cardView2.alpha = 0f
+
+        val fadeInAnimator1 = ObjectAnimator.ofFloat(cardView1, "alpha", 0f, 1f)
+        fadeInAnimator1.duration = ANIMATION_DURATION
+        val fadeInAnimator2 = ObjectAnimator.ofFloat(cardView2, "alpha", 0f, 1f)
+        fadeInAnimator2.duration = ANIMATION_DURATION
+
+        val animator1 = ObjectAnimator.ofFloat(cardView1, "translationY", -30f, 10f)
+        animator1.duration = ANIMATION_DURATION
+        val animator2 = ObjectAnimator.ofFloat(cardView2, "translationY", -30f, 10f)
+        animator2.duration = ANIMATION_DURATION
+
+        animator1.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                animator2.start()
+                fadeInAnimator2.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+
+        fadeInAnimator1.start()
+        animator1.start()
+    }
+
 }
